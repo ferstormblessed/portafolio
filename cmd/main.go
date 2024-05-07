df5e7f44518b6e55590c4545dda1c23af48c03e1
Fernando Camacho <salvadorfernando2002@outlook.com>
2024-05-07 19:31:51 +0200

    the data structure for the text content and how the server passes the data to the templates complete

--- cmd/main.go
+++ cmd/main.go
@@ -24,31 +24,54 @@ func NewTemplates() *Templates {
 	}
 }
 
+type Text struct {
+	Title  string
+	Middle []string
+	Bottom string
+}
+
+func newText() Text {
+	return Text{
+		Title:  "",
+		Middle: make([]string, 0),
+		Bottom: "",
+	}
+}
+
 type Data struct {
-	Text []string
+	Text Text
 }
 
 func newData() Data {
 	return Data{
-		Text: make([]string, 0),
+		Text: newText(),
 	}
 }
 
 func (d *Data) getContentText(filepath string) {
-    file, err := os.Open(filepath)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        d.Text = append(d.Text, scanner.Text())
-    }
+	scanner := bufio.NewScanner(file)
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
+	// read all the lines of file
+	tmp_text := make([]string, 0)
+	for scanner.Scan() {
+		tmp_text = append(tmp_text, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	d.Text.Title = tmp_text[0]
+	d.Text.Bottom = tmp_text[len(tmp_text)-1]
+	for i := 1; i < len(tmp_text)-1; i++ {
+		d.Text.Middle = append(d.Text.Middle, tmp_text[i])
+	}
 }
 
 func main() {
@@ -58,9 +81,9 @@ func main() {
 	e.Static("/images", "images")
 	e.Static("/css", "css")
 
-    data := newData()
-    filepath := "text/content.md"
-    data.getContentText(filepath)
+	data := newData()
+	filepath := "text/content.md"
+	data.getContentText(filepath)
 
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(200, "index", data)
